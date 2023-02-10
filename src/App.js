2c4d9b64f79a63c8a15446acec57313cb460c945
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-10 11:29:53 -0600

    html structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import GameDataProvider from "./context/gameDataContext";
 function App() {
   return (
     <GameDataProvider>
-      <div className="App"></div>
+      <div className="connect-four">
+        <GameScreen />
+      </div>
     </GameDataProvider>
   );
 }
--- src/screens/game.js
+++ src/screens/game.js
@@ -1,9 +1,21 @@
 const GameScreen = () => {
+  const numberOfRows = 7;
+
+  const numberOfCols = 6;
   return (
     <div className="game-screen">
       <div className="board">
         <div className="board-back"></div>
-        <div className="board-top"></div>
+        <div className="board-top">
+          <div className="board-row"></div>
+          <div className="board-row"></div>
+          <div className="board-row"></div>
+          <div className="board-row"></div>
+          <div className="board-row"></div>
+          <div className="board-row"></div>
+          <div className="board-row"></div>
+          <div className="board-row"></div>
+        </div>
       </div>
     </div>
   );
