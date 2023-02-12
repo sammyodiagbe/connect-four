f12a0b1136fc9103d360675075c4d7476a44bf30
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-12 16:05:36 -0600

    bug fixed

--- src/context/gameDataContext.js
+++ src/context/gameDataContext.js
@@ -68,24 +68,24 @@ const GameDataProvider = ({ children }) => {
     // diagonals
 
     const diagonalUpRight = [
-      validValue(y + 1, x + 1),
-      validValue(y + 1, x + 2),
-      validValue(y + 1, x + 3),
+      validValue(y - 1, x + 1),
+      validValue(y - 1, x + 2),
+      validValue(y - 1, x + 3),
     ];
     const diagonalUpLeft = [
-      validValue(y + 1, x - 1),
-      validValue(y + 1, x - 2),
-      validValue(y + 1, x - 3),
+      validValue(y - 1, x - 1),
+      validValue(y - 1, x - 2),
+      validValue(y - 1, x - 3),
     ];
     const diagonalDownRight = [
-      validValue(y - 1, x + 1),
-      validValue(y - 2, x + 2),
-      validValue(y - 3, x + 3),
+      validValue(y + 1, x + 1),
+      validValue(y + 2, x + 2),
+      validValue(y + 3, x + 3),
     ];
     const diagonalDownLeft = [
-      validValue(y - 1, x - 1),
-      validValue(y - 2, x - 2),
-      validValue(y - 3, x - 3),
+      validValue(y + 1, x - 1),
+      validValue(y + 2, x - 2),
+      validValue(y + 3, x - 3),
     ];
 
     const checkValue = (y, x, arr) => {
